5e368c1adb3e813b492cdff09aa7944fb0c318cf
rajshekhar23 <rajshekharbudharam2015@gmail.com>
2018-03-08 19:55:42 +0530

    updated test issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
   public isOpened: boolean;
   public inputSearch: any;
 
-  constructor() {
+  constructor(private router: Router) {
     this.isOpened = false;
   }
   search() {
     this.inputSearch = '';
     this.isOpened = !this.isOpened;
   }
+
+  ngOnInit() {
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+        return;
+      }
+      window.scrollTo(0, 0)
+    });
+  }
+
 }
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,42 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiServicesService } from '../api-services.service';
-import { IProductCategories } from '../model/IProductCategories';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ApiServicesService } from "../api-services.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css'],
+  selector: "app-footer",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.css"],
   providers: [ApiServicesService]
 })
 export class FooterComponent implements OnInit {
-  _allProductCategories: IProductCategories[];
+  _allProductCategories: any;
   _contactDetails: any;
 
-  constructor(private _apiService: ApiServicesService, private _router: Router) { }
+  constructor(
+    private _apiService: ApiServicesService,
+    private _router: Router
+  ) {}
 
   getAllProductCategories(): void {
-    this._apiService.getAllProductCategories()
-      .subscribe(result => {
-        result.forEach(function (res, index) {
-          res['imgUrl'] = '../../../assets/images/' + (index + 1) + '.jpg';
-        })
-        this._allProductCategories = result
+    this._apiService.getAllProductCategories().subscribe(
+      result => {
+        result.forEach(function(res, index) {
+          res["imgUrl"] = "../../../assets/images/" + (index + 1) + ".jpg";
+        });
+        this._allProductCategories = result;
       },
-      error => console.log('Error :: ' + error))
+      error => console.log("Error :: " + error)
+    );
   }
 
   getContactDetails(): void {
-    this._apiService.getContactDetails()
-      .subscribe(result => {
-        this._contactDetails = result
+    this._apiService.getContactDetails().subscribe(
+      result => {
+        this._contactDetails = result;
       },
-      error => console.log('Error :: ' + error))
+      error => console.log("Error :: " + error)
+    );
   }
 
   ngOnInit() {
     this.getAllProductCategories();
     this.getContactDetails();
   }
-
 }
